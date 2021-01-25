f59f80196cb5a66612d301c136ac5c32a2933203
kbwoo00 <59406944+kbwoo00@users.noreply.github.com>
2021-01-25 20:20:52 +0900

    StackOverflow extract job

--- stackoverflow.py
+++ stackoverflow.py
@@ -2,14 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-URL = f"https://stackoverflow.com/jobs?q=python&pg=2"
+URL = f"https://stackoverflow.com/jobs?q=python"
 
 def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
   pages = soup.find("div",{"class":"s-pagination"}).find_all("a")
-  print(pages)
+  return int(last_page)
+
+def extract_job(html):
+  title = html.find("h2",{"class":"mb4"}).find("a")["title"]
+  company, location = html.find("h3",{"class":"fs-body1"}).find_all("span", recursive = False)
+  company = company.get_text(strip=True) 
+  location = location.get_text(strip=True)
+  return{'title':title, 'company':company, 'location':location}
+
+def extract_jobs(last_page) :
+  jobs = []
+  for page in range(last_page):
+    result = requests.get(f"{URL}&pg={page+1}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div",{"class":"-job"})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
+  return jobs
 
 def get_jobs():
   last_page = get_last_page()
-  return []
+  jobs = extract_jobs(last_page)
+  return jobs
